cec4af167e8945029bc0e57b389ca4c43eb747bc
Rick <rmansfield@realpeoplesratings.com>
2021-07-14 19:57:54 -0500

    U2.2 M3 Gabes Guide add fetchFriends App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,27 +4,17 @@ import axios from 'axios';
 // 👉 TASK 2 - import the contants from constants/index.js
 import {BASE_URL, API_KEY} from '../constants/index';
 
-
 import Details from './Details';
+
+
 
 export default function App() {
   const [friends, setFriends] = useState([]);
   const [currentFriendId, setCurrentFriendId] = useState(null);
-
-  useEffect(() => {
-    axios.get(`${BASE_URL}/friends?api_key=${API_KEY}`)
-    .then(res => {
-      debugger
-    })
-    .catch(err => {
-      debugger
-    })
-  }, []);
-
   const openDetails = id => {
     setCurrentFriendId(id)
   }
-
+  
   const closeDetails = () => {
     setCurrentFriendId(null)
   }
@@ -33,6 +23,21 @@ export default function App() {
   // caused by the first render only. You'll need `useEffect` from React.
   // The effect should consist of a call to the API using axios.
   // On success, set the array of friend objects from the API into state.
+
+  useEffect(() => {
+    const fetchFriends = () =>{
+    axios.get(`${BASE_URL}/friends?api_key=${API_KEY}`)
+    .then(res => {
+      setFriends(res.data);
+    })
+    .catch(err => {
+      debugger
+    });
+  };
+  fetchFriends();
+  }, []);
+
+
 
   const Friend = props => (
     <div className='friend'>
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 // 👉 Importing React libs from node_modules folder
-import React from 'react'
-import { render } from 'react-dom'
+import React from 'react';
+import { render } from 'react-dom';
 
 // 👉 Importing a React component from another file
 import App from './components/App'
 
 // 👉 Importing our styles
-import './styles.less'
+import './styles.less';
 
 render(
   <App />,
